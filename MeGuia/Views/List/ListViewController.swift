1b82566e6d6728cab0a1c76034de1f5130eb8610
Joao Henrique Rodrigues <joaohrodrigues@MacBook-Air.local>
2023-12-05 12:00:08 -0300

    FEAT: search bar first version

--- MeGuia/Views/List/ListViewController.swift
+++ MeGuia/Views/List/ListViewController.swift
@@ -6,6 +6,7 @@ protocol ListPresenterProtocol {
 
 final class ListViewController: UIViewController {
     private var cellModels: [CellModel] = []
+    private var filteredCellModels: [CellModel] = []
     
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -48,6 +49,10 @@ final class ListViewController: UIViewController {
         
         // Setup View
         view.backgroundColor = .systemBackground
+        
+        // Setup SearchBar
+        setupSearchBar()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -59,6 +64,8 @@ final class ListViewController: UIViewController {
             switch result {
                 case .success(let cellModels):
                     self?.cellModels = cellModels
+                //CRIANDO UMA CÓPIA DO CELL MODELS P/ USAR A LISTA DE ELEMENTOS FILTRADOS
+                    self?.filteredCellModels = cellModels
                     self?.tableView.reloadData()
                 case .failure:
                     print("Fazer tratativa para o cenário de error ❌")
@@ -84,23 +91,63 @@ final class ListViewController: UIViewController {
             spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
+    
+    private func setupSearchBar() {
+        // Configurando o searchController no navigationItem
+        navigationItem.searchController = UISearchController()
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        // Configurando a SearchBar do SearchController posicinado no navigationItem
+        navigationItem.searchController?.searchBar.delegate = self
+        navigationItem.searchController?.searchBar.sizeToFit()
+        navigationItem.searchController?.searchBar.searchBarStyle = .minimal
+        navigationItem.searchController?.searchBar.placeholder = "Pesquisar"
+    }
 }
 
 extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ListCell.identifier) as? ListCell else { return UITableViewCell() }
-        let cellModel = cellModels[indexPath.row]
+        //let cellModel = cellModels[indexPath.row]
+        
+        //PASSANDO A LISTA filteredCellModels como base de dados para a Table View
+        let cellModel = filteredCellModels[indexPath.row]
         cell.configure(with: cellModel)
 
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cellModels.count
+        //return cellModels.count
+        
+        //CONTANDO A QUANTIDADE DE ELEMENTOS NA lista de elementos filtrados
+        return filteredCellModels.count
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailViewController = DetailViewController(cellModel: cellModels[indexPath.row])
         navigationController?.pushViewController(detailViewController, animated: true)
+    }
+}
+
+extension ListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        self.filteredCellModels = []
+        
+        if searchText.isEmpty {
+            self.filteredCellModels = self.cellModels
+        } else {
+            for experience in cellModels {
+                if experience.title.lowercased().contains(searchText.lowercased()) {
+                    filteredCellModels.append(experience)
+                }
+            }
+        }
+        self.tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
     }
 }
